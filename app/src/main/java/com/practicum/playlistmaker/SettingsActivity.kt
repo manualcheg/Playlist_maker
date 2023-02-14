03bc98c4929a2b1319cd6436659eb9ae385ffe1b
Andrei Kochubeev <manualcheg@yandex.ru>
2023-02-14 19:00:23 +0300

    Спринт 9/10 → Тема 2/5: Context и Intent → Урок 3/3 1. Вынесены строковые ресурсы в файл strings.xml 2. Реализована кнопка "Поделиться приложением" 3. Реализована кнопка "Написать в поддержку" 4. Реализована кнопка "Пользовательское соглашение"

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,34 +1,80 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Switch
-import android.widget.Toolbar
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val arrowSettingsBack = findViewById<ImageView>(R.id.settings_arrow_back_like_button)
+        val arrowSettingsBack = findViewById<ImageView>(R.id.settings_screen_arrow_back_like_button)
         arrowSettingsBack.setOnClickListener{
-                this.onStop()
+                this.finish()
         }
 
-        val switch = findViewById<Switch>(R.id.switch1)
-        switch.setOnCheckedChangeListener { _, isChecked ->
+        val switch_night_theme = findViewById<Switch>(R.id.switch1)
+//        Попытка проверки на включенность темной темы
+/*        if (getResources().getConfiguration().isNightModeActive){
+            switch_night_theme.isChecked
+        }*/
+
+        when ((resources.configuration.uiMode)) {
+            Configuration.UI_MODE_NIGHT_YES -> switch_night_theme.isChecked
+            Configuration.UI_MODE_NIGHT_NO -> return
+        }
+        switch_night_theme.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
-
         }
 
+//      Кнопка "Пользовательское соглашение" с переходом на страницу
+        val userAgreementTextView = findViewById<TextView>(R.id.settings_screen_user_agreement_textview)
+        userAgreementTextView.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/")))
+        }
+
+//        Кнопка "Поделится приложением"
+        val shareApp = findViewById<TextView>(R.id.settings_screen_shareapp_textview)
+        shareApp.setOnClickListener{
+            val shareIntent = Intent.createChooser(Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/android-developer/")
+            }, null)
+            startActivity(shareIntent)
+        }
+
+//        Кнопка "Написать в поддержку"
+        val emailToSupport = findViewById<TextView>(R.id.settings_screen_send_mail_support_textview)
+        emailToSupport.setOnClickListener{
+            val sendEmail = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("manualcheg@yandex.ru"))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.text_mail_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.text_mail_body)) }
+            startActivity(sendEmail)
+        }
+    }
+
+    //попытка создания метода, который проверяет включенность тёмной темы
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun nightThemeCheck(switch:Switch){
+        if (getResources().getConfiguration().isNightModeActive){
+            switch.isChecked
+        }
     }
 }
