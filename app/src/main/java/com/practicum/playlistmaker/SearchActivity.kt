41c6fe1e7ec3f578dad9f2bfc441da6e66e7b9ea
Andrei Kochubeev <manualcheg@yandex.ru>
2023-02-16 13:04:38 +0300

    Спринт 9/10 → Тема 3/5: EditText и TextWatcher → Урок 3/3 1. Создал вёрстку для SearchActivity 2. Реализовал логику кнопки сброса поисковой строки. 3. Добавил новый TextWatcher для поисковой строки 4. Установил верные атрибуты для EditText: hint, maxLines и прочие. 5. Установил inputType.

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,11 +1,63 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val editTextSearchActivity = findViewById<EditText>(R.id.search_activity_edittext)
+        val searchClearEdittextImageview =
+            findViewById<ImageView>(R.id.search_clear_edittext_imageview)
+        val settingsArrowBack = findViewById<androidx.appcompat.widget.Toolbar>(R.id.search_activity_toolbar)
+
+        searchClearEdittextImageview.setOnClickListener {
+            editTextSearchActivity.setText("")
+            val view: View? = this.currentFocus
+            if (view != null) {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(view.windowToken, 0)
+            }
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchClearEdittextImageview.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        editTextSearchActivity.addTextChangedListener(simpleTextWatcher)
+        settingsArrowBack.setNavigationOnClickListener{
+            this.finish()
+        }
     }
 }
+
+private fun clearButtonVisibility(s: CharSequence?): Int {
+    return if (s.isNullOrEmpty()) {
+        View.GONE
+    } else {
+        View.VISIBLE
+    }
+}
+
+
+
+
