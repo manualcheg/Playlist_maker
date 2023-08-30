e726f3e4ee99350dd53f7b3c1708188db777cca1
manualcheg <manualcheg@yandex.ru>
2023-08-30 21:27:21 +0300

    Sprint_22_playlist db works grid recycleView works

---
+++ app/src/main/java/com/practicum/playlistmaker/mediateka/playlists/presentation/PlaylistViewHolder.kt
@@ -0,0 +1,28 @@
+package com.practicum.playlistmaker.mediateka.playlists.presentation
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.mediateka.playlists.domain.entities.Playlist
+
+class PlaylistViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val image: ImageView = itemView.findViewById(R.id.playlistGridItemImageView)
+    private val playlistName: TextView = itemView.findViewById(R.id.playlistGridItemTextViewPlaylistName)
+    private val countOfTracks: TextView = itemView.findViewById(R.id.playlistGridItemTextViewCountOfTracks)
+
+    fun bind(playlist:Playlist){
+        playlistName.text = playlist.playlistName
+        countOfTracks.text = playlist.countOfTracks.toString()
+
+        Glide.with(image)
+            .load(playlist.playlistCover)
+            .placeholder(R.drawable.placeholder_album_cover)
+            .circleCrop()
+            .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.dp8)))
+            .into(image)
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/mediateka/playlists/presentation/ui/PlaylistsFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/mediateka/playlists/presentation/ui/PlaylistsFragment.kt
@@ -6,15 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.practicum.playlistmaker.R
 import com.practicum.playlistmaker.databinding.FragmentPlaylistsBinding
+import com.practicum.playlistmaker.mediateka.favourites.domain.FavouritesState
+import com.practicum.playlistmaker.mediateka.playlists.domain.PlaylistsState
 import com.practicum.playlistmaker.mediateka.playlists.domain.entities.Playlist
+import com.practicum.playlistmaker.mediateka.playlists.presentation.PlaylistAdapter
 import com.practicum.playlistmaker.mediateka.playlists.presentation.viewmodels.PlaylistsFragmentViewModel
+import com.practicum.playlistmaker.search.domain.entities.Track
+import com.practicum.playlistmaker.search.presentation.ui.SearchAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistsFragment : Fragment() {
     private val playlistsFragmentViewModel: PlaylistsFragmentViewModel by viewModel()
-    private val playlists: List<Playlist> = ArrayList()
+    private val playlists: ArrayList<Playlist> = ArrayList()
+
+    //    private val playlistAdapter = PlaylistAdapter(playlists)
+    private var playlistAdapter = PlaylistAdapter(playlists)
 
     private lateinit var binding: FragmentPlaylistsBinding
 
@@ -31,6 +40,13 @@ class PlaylistsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         playlistsFragmentViewModel.getPlaylists()
+        binding.recyclerViewPlaylist.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.recyclerViewPlaylist.adapter = playlistAdapter
+
+        playlistsFragmentViewModel.getPlaylists()
+        playlistsFragmentViewModel.stateLiveData().observe(viewLifecycleOwner) { state ->
+            render(state)
+        }
 
         binding.fragmentFavouritesButtonCreatePlaylist.setOnClickListener {
 /*            parentFragment?.parentFragmentManager?.commit {
@@ -40,5 +56,26 @@ class PlaylistsFragment : Fragment() {
             }*/
             findNavController().navigate(R.id.action_mediatekaFragment_to_playlistCreateFragment)
         }
+    }
+
+    private fun render(state: PlaylistsState) {
+        when (state) {
+            is PlaylistsState.Empty -> showEmpty()
+            is PlaylistsState.Content -> showContent(state.playlists)
+        }
+    }
+
+    private fun showContent(playlists: List<Playlist>) {
+        playlistAdapter = PlaylistAdapter(playlists as MutableList<Playlist>)
+        binding.recyclerViewPlaylist.adapter = playlistAdapter
+        playlistAdapter!!.notifyItemRangeChanged(0, playlists!!.lastIndex)
+
+        binding.recyclerViewPlaylist.visibility = View.VISIBLE
+        binding.placeholderFragmentPlaylistThereIsNothing.visibility = View.GONE
+    }
+
+    private fun showEmpty() {
+        binding.recyclerViewPlaylist.visibility = View.GONE
+        binding.placeholderFragmentPlaylistThereIsNothing.visibility = View.VISIBLE
     }
 }
